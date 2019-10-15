d246c5b7523a68340469c4e6a6a1ec9501e1404c
Kelly Meier <kemeier2000@yahoo.com>
2019-10-15 14:39:07 -0500

    added .gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 //contains mysql connection data
 var mysql = require('mysql');
-var connection = mysql.createConnection ({
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+connection = mysql.createConnection ({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "9tachyons",
     database: "burgers_db"
 });
+}
 
 connection.connect(function(err)    {
     if (err)throw err;    {
