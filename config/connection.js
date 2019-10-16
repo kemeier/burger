ffc96394c7a96587dc8adf507ee644bf81b7a8d2
Kelly Meier <kemeier2000@yahoo.com>
2019-10-15 21:45:05 -0500

    added jaws-db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 //contains mysql connection data
 var mysql = require('mysql');
-var connection = mysql.createConnection ({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "9tachyons",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '9tachyons',
+        database: 'burgers_db'
+    })
+};
 
 connection.connect(function(err)    {
     if (err) {
